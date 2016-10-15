e6bddc7525f011889283bbb72d91314414ce4683
PikalaxALT <pikalaxalt@gmail.com>
2016-10-15 17:44:34 -0400

    Rip some graphics (battle objects?)

---
+++ pics.asm
@@ -0,0 +1,388 @@
+INCLUDE "includes.asm"
+
+SECTION "bank 41", ROMX, BANK [DENJUU_00]
+DenjuPic_064::
+	dr $104000, $104100
+
+DenjuPic_065::
+	dr $104100, $104200
+
+DenjuPic_066::
+	dr $104200, $104300
+
+DenjuPic_067::
+	dr $104300, $104400
+
+DenjuPic_068::
+	dr $104400, $104500
+
+DenjuPic_069::
+	dr $104500, $104600
+
+DenjuPic_070::
+	dr $104600, $104700
+
+DenjuPic_071::
+	dr $104700, $104800
+
+DenjuPic_072::
+	dr $104800, $104900
+
+DenjuPic_073::
+	dr $104900, $104a00
+
+DenjuPic_074::
+	dr $104a00, $104b00
+
+DenjuPic_075::
+	dr $104b00, $104c00
+
+DenjuPic_076::
+	dr $104c00, $104d00
+
+DenjuPic_077::
+	dr $104d00, $104e00
+
+DenjuPic_078::
+	dr $104e00, $104f00
+
+DenjuPic_079::
+	dr $104f00, $105000
+
+DenjuPic_080::
+	dr $105000, $105100
+
+DenjuPic_081::
+	dr $105100, $105200
+
+DenjuPic_082::
+	dr $105200, $105300
+
+DenjuPic_083::
+	dr $105300, $105400
+
+DenjuPic_084::
+	dr $105400, $105500
+
+DenjuPic_085::
+	dr $105500, $105600
+
+DenjuPic_086::
+	dr $105600, $105700
+
+DenjuPic_087::
+	dr $105700, $105800
+
+DenjuPic_088::
+	dr $105800, $105900
+
+DenjuPic_089::
+	dr $105900, $105a00
+
+DenjuPic_090::
+	dr $105a00, $105b00
+
+DenjuPic_091::
+	dr $105b00, $105c00
+
+DenjuPic_092::
+	dr $105c00, $105d00
+
+DenjuPic_093::
+	dr $105d00, $105e00
+
+DenjuPic_094::
+	dr $105e00, $105f00
+
+DenjuPic_095::
+	dr $105f00, $106000
+
+DenjuPic_096::
+	dr $106000, $106100
+
+DenjuPic_097::
+	dr $106100, $106200
+
+DenjuPic_098::
+	dr $106200, $106300
+
+DenjuPic_099::
+	dr $106300, $106400
+
+DenjuPic_100::
+	dr $106400, $106500
+
+DenjuPic_101::
+	dr $106500, $106600
+
+DenjuPic_102::
+	dr $106600, $106700
+
+DenjuPic_103::
+	dr $106700, $106800
+
+DenjuPic_104::
+	dr $106800, $106900
+
+DenjuPic_105::
+	dr $106900, $106a00
+
+DenjuPic_106::
+	dr $106a00, $106b00
+
+DenjuPic_107::
+	dr $106b00, $106c00
+
+DenjuPic_108::
+	dr $106c00, $106d00
+
+DenjuPic_109::
+	dr $106d00, $106e00
+
+DenjuPic_110::
+	dr $106e00, $106f00
+
+DenjuPic_111::
+	dr $106f00, $107000
+
+DenjuPic_112::
+	dr $107000, $107100
+
+DenjuPic_113::
+	dr $107100, $107200
+
+DenjuPic_114::
+	dr $107200, $107300
+
+DenjuPic_115::
+	dr $107300, $107400
+
+DenjuPic_116::
+	dr $107400, $107500
+
+DenjuPic_117::
+	dr $107500, $107600
+
+DenjuPic_118::
+	dr $107600, $107700
+
+DenjuPic_119::
+	dr $107700, $107800
+
+DenjuPic_120::
+	dr $107800, $107900
+
+DenjuPic_121::
+	dr $107900, $107a00
+
+DenjuPic_122::
+	dr $107a00, $107b00
+
+DenjuPic_123::
+	dr $107b00, $107c00
+
+DenjuPic_124::
+	dr $107c00, $107d00
+
+DenjuPic_125::
+	dr $107d00, $107e00
+
+DenjuPic_126::
+	dr $107e00, $107f00
+
+DenjuPic_127::
+	dr $107f00, $108000
+
+SECTION "bank 42", ROMX, BANK [DENJUU_01]
+DenjuPic_128::
+	dr $108000, $108100
+
+DenjuPic_129::
+	dr $108100, $108200
+
+DenjuPic_130::
+	dr $108200, $108300
+
+DenjuPic_131::
+	dr $108300, $108400
+
+DenjuPic_132::
+	dr $108400, $108500
+
+DenjuPic_133::
+	dr $108500, $108600
+
+DenjuPic_134::
+	dr $108600, $108700
+
+DenjuPic_135::
+	dr $108700, $108800
+
+DenjuPic_136::
+	dr $108800, $108900
+
+DenjuPic_137::
+	dr $108900, $108a00
+
+DenjuPic_138::
+	dr $108a00, $108b00
+
+DenjuPic_139::
+	dr $108b00, $108c00
+
+DenjuPic_140::
+	dr $108c00, $108d00
+
+DenjuPic_141::
+	dr $108d00, $108e00
+
+DenjuPic_142::
+	dr $108e00, $108f00
+
+DenjuPic_143::
+	dr $108f00, $109000
+
+DenjuPic_144::
+	dr $109000, $109100
+
+DenjuPic_145::
+	dr $109100, $109200
+
+DenjuPic_146::
+	dr $109200, $109300
+
+DenjuPic_147::
+	dr $109300, $109400
+
+DenjuPic_148::
+	dr $109400, $109500
+
+DenjuPic_149::
+	dr $109500, $109600
+
+DenjuPic_150::
+	dr $109600, $109700
+
+DenjuPic_151::
+	dr $109700, $109800
+
+DenjuPic_152::
+	dr $109800, $109900
+
+DenjuPic_153::
+	dr $109900, $109a00
+
+DenjuPic_154::
+	dr $109a00, $109b00
+
+DenjuPic_155::
+	dr $109b00, $109c00
+
+DenjuPic_156::
+	dr $109c00, $109d00
+
+DenjuPic_157::
+	dr $109d00, $109e00
+
+DenjuPic_158::
+	dr $109e00, $109f00
+
+DenjuPic_159::
+	dr $109f00, $10a000
+
+DenjuPic_160::
+	dr $10a000, $10a100
+
+DenjuPic_161::
+	dr $10a100, $10a200
+
+DenjuPic_162::
+	dr $10a200, $10a300
+
+DenjuPic_163::
+	dr $10a300, $10a400
+
+DenjuPic_164::
+	dr $10a400, $10a500
+
+DenjuPic_165::
+	dr $10a500, $10a600
+
+DenjuPic_166::
+	dr $10a600, $10a700
+
+DenjuPic_167::
+	dr $10a700, $10a800
+
+DenjuPic_168::
+	dr $10a800, $10a900
+
+DenjuPic_169::
+	dr $10a900, $10aa00
+
+DenjuPic_170::
+	dr $10aa00, $10ab00
+
+DenjuPic_171::
+	dr $10ab00, $10ac00
+
+DenjuPic_172::
+	dr $10ac00, $10ad00
+
+DenjuPic_173::
+	dr $10ad00, $10ae00
+
+DenjuPic_174::
+	dr $10ae00, $10af00
+
+DenjuPic_175::
+	dr $10af00, $10b000
+
+DenjuPic_176::
+	dr $10b000, $10b100
+
+DenjuPic_177::
+	dr $10b100, $10b200
+
+DenjuPic_178::
+	dr $10b200, $10b300
+
+DenjuPic_179::
+	dr $10b300, $10b400
+
+DenjuPic_180::
+	dr $10b400, $10b500
+
+DenjuPic_181::
+	dr $10b500, $10b600
+
+DenjuPic_182::
+	dr $10b600, $10b700
+
+DenjuPic_183::
+	dr $10b700, $10b800
+
+DenjuPic_184::
+	dr $10b800, $10b900
+
+DenjuPic_185::
+	dr $10b900, $10ba00
+
+DenjuPic_186::
+	dr $10ba00, $10bb00
+
+DenjuPic_187::
+	dr $10bb00, $10bc00
+
+DenjuPic_188::
+	dr $10bc00, $10bd00
+
+DenjuPic_189::
+	dr $10bd00, $10be00
+
+DenjuPic_190::
+	dr $10be00, $10bf00
+
+DenjuPic_191::
+	dr $10bf00, $10c000
+
